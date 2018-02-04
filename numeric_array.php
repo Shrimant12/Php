e369c89ae9ecdb85d6c8f8b961c6f86a1b9c7650
root <root@shri.com>
2018-02-04 23:51:01 +0530

    Numeric arrays in php Example

---
+++ numeric_array.php
@@ -0,0 +1,34 @@
+//Numeric arrays in php Example.
+
+<?php
+
+$names = array("Shrimant","Steve","Elon","Mark","Billy");
+
+echo "<br>$names[0]";
+ echo "<br>$names[1]";
+  echo "<br>$names[2]";
+    echo "<br>$names[3]";
+     echo "<br>$names[4]";	
+      echo "<br>";
+	
+$clg[0]="ADCET";
+ $clg[1]="MIT";
+  $clg[2]="IIT";
+   $clg[3]="IIS";
+    $clg[4]="NIT";
+     echo "<br>";
+
+echo "<br>$clg[0]";
+ echo "<br>$clg[1]";
+  echo "<br>$clg[2]";
+   echo "<br>$clg[3]";
+    echo "<br>$clg[4]";
+     echo "<br><br>";
+
+print_r($names);
+
+echo "<br><br>";
+
+print_r($clg);
+?>
+
